fc1eb006a2e7349939acf60394ae54a5f6a0e3db
Mister-Gee <Fakuadegbenga@gmail.com>
2022-11-07 07:56:24 +0100

    modules added

--- src/pages/projectDetail/ProjectDetails.tsx
+++ src/pages/projectDetail/ProjectDetails.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
 import { useNavigate } from "react-router-dom";
 
@@ -31,10 +32,40 @@ const ProjectDetails = () => {
               </div>
             </div>
           </div>
-          <div className="project-body">
-            <img src="assets/images/project-detail.png" />
-            <div className="detail-breakdown">
-              <div className="organization">Organization</div>
+          <div className="project-body-wrapper">
+            <div className="project-body">
+              <img src="assets/images/project-detail.png" />
+              <div className="detail-breakdown">
+                <div className="organization">Organization</div>
+                <div className="detail">
+                  <img src="assets/images/organisation.png" />
+                  <div className="breakdown">
+                    <div className="summary">We love Japan Organization </div>
+                    <div className="info">
+                      We love Japan Organization aim to used blockchain technlog to build a small buiness and promote jp
+                      culture xxxxxxxxxxxxxxxxxxxxx......
+                    </div>
+                    <div className="learn-more">Learn more{">>>"}</div>
+                  </div>
+                </div>
+                <div className="obj-backer">
+                  <div className="objective">
+                    <div className="key">Objective:</div>
+                    <div className="value">
+                      <img src="assets/images/USDC.svg" /> <span>00000000</span>
+                    </div>
+                    <Button variant="danger">Contribute</Button>
+                  </div>
+                  <span className="iconify" data-icon="fluent:divider-short-20-regular"></span>
+                  <div className="backer">
+                    <div className="key">Number of Backers: : </div>
+                    <div className="value">
+                      <img src="assets/images/users.svg" /> <span>00000000</span>
+                    </div>
+                    <Button variant="success">Vote Now</Button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <div></div>
